0df1a93446fd988f601bc770fb68d74216e85cfe
AmanKhan-Dev <amankhan7058int@gmail.com>
2025-01-14 17:53:56 +0530

    done

--- src/main/java/com/quizApp/Backend/MainAppClass/config/WebConfig.java
+++ src/main/java/com/quizApp/Backend/MainAppClass/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // React app URL
+                .allowedOrigins("https://bitcodemaster.netlify.app/") // React app URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
